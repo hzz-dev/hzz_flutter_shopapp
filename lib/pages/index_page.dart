5b2aa5f1e0568cf118a1ff0e0f4c41b05cb3fe20
丰台_吴彦祖 <371251480@qq.com>
2020-12-07 16:25:51 +0800

    完成导航和底部tabbar

---
+++ lib/pages/index_page.dart
@@ -0,0 +1,81 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'cart/cart_page.dart';
+import 'home/home_page.dart';
+import 'category/category_page.dart';
+import 'member/member_page.dart';
+
+class IndexPage extends StatefulWidget {
+  @override
+  _IndexPageState createState() => _IndexPageState();
+}
+
+class _IndexPageState extends State<IndexPage> {
+  // 底部tabbar
+  final List<BottomNavigationBarItem> bottomTabs = [
+    BottomNavigationBarItem(
+      icon: Icon(CupertinoIcons.home),
+      // ignore: deprecated_member_use
+      title: Text('首页'),
+    ),
+    BottomNavigationBarItem(
+      icon: Icon(CupertinoIcons.search),
+      // ignore: deprecated_member_use
+      title: Text('分类'),
+    ),
+    BottomNavigationBarItem(
+      icon: Icon(CupertinoIcons.shopping_cart),
+      // ignore: deprecated_member_use
+      title: Text('购物车'),
+    ),
+    BottomNavigationBarItem(
+      icon: Icon(CupertinoIcons.profile_circled),
+      // ignore: deprecated_member_use
+      title: Text('会员中心'),
+    )
+  ];
+
+  final List tabBodies = [HomePage(), CategoryPage(), CartPage(), MemberPage()];
+
+  int currentIndex = 0;
+  var currentPage;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    // 初始化
+    currentPage = tabBodies[currentIndex];
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color.fromRGBO(244, 245, 245, 1.0),
+      bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed,
+        currentIndex: currentIndex,
+        items: bottomTabs,
+        onTap: (index) {
+          setState(() {
+            currentIndex = index;
+            currentPage = tabBodies[currentIndex];
+          });
+        },
+      ),
+      body: currentPage,
+    );
+  }
+}
+
+// class IndexPage extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(title: Text('伏兔科技')),
+//       body: Center(
+//         child: Text('伏兔科技'),
+//       ),
+//     );
+//   }
+// }
