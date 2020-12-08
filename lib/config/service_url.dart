b5c3dcb3c0c0218239920c3c1b19d6f10c78bb16
丰台_吴彦祖 <371251480@qq.com>
2020-12-08 17:30:08 +0800

    首页轮播图flutter_swipper和适配空间screeUtil使用

---
+++ lib/config/service_url.dart
@@ -0,0 +1,11 @@
+// const serviceUrl= 'http://v.jspang.com:8088/baixing/';
+// const serviceUrl = 'https://wxmini.baixingliangfan.cn/47.104.152.37/';
+const serviceUrl =
+    'https://www.fastmock.site/mock/2c00e190d27ec9fef9feb0ad9e03b234/flutter_hzz_shop/';
+const servicePath = {
+  'homePageContext': serviceUrl + 'wxmini/homePageContent', // 商家首页信息
+  'homePageBelowConten': serviceUrl + 'wxmini/homePageBelowConten', //商城首页热卖商品拉取
+  'getCategory': serviceUrl + 'wxmini/getCategory', //商品类别信息
+  'getMallGoods': serviceUrl + 'wxmini/getMallGoods', //商品分类的商品列表
+  'getGoodDetailById': serviceUrl + 'wxmini/getGoodDetailById', //商品详细信息列表
+};
--- lib/pages/home/home_page.dart
+++ lib/pages/home/home_page.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
-import "package:dio/dio.dart";
-import '../../config/httpHeaders.dart';
+import '../../service/service_method.dart';
+// ignore: unused_import
+import 'package:flutter_swiper/flutter_swiper.dart';
+import 'dart:convert';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class HomePage extends StatefulWidget {
   @override
@@ -8,52 +11,117 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  String showText = '还没有请求数据';
+  String homePageContent = '正在获取数据';
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Scaffold(
-          appBar: AppBar(title: Text('请求远程数据')),
-          // SingleChildScrollView 解决键盘或者其他滚动会有bug
-          body: SingleChildScrollView(
-            child: Column(
+    return Scaffold(
+      appBar: AppBar(title: Text('百姓生活+')),
+      body: FutureBuilder(
+        future: getHomePageContent(),
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            // var data = json.decode(snapshot.data.toString());
+            var data = snapshot.data;
+            List swiperDataList = data['data']['slides'];
+            print(swiperDataList);
+            // List<Map> swiperDataList =
+            //     (data['data']['slides'] as RList).cast(); // 顶部轮播组件数
+            return Column(
               children: <Widget>[
-                RaisedButton(
-                  onPressed: _jike,
-                  child: Text('请求数据'),
-                ),
-                Text(showText),
+                SwiperDiy(swiperDataList: swiperDataList),
               ],
-            ),
-          )),
+            );
+            // return Text('qsdasdsa');
+          } else {
+            return Center(
+              child: Text('加载中...'),
+            );
+          }
+        },
+      ),
     );
   }
+}
 
-  void _jike() {
-    print('开始向极客时间请求数据');
-    getHttp().then((value) {
-      setState(() {
-        print(value);
-        showText = value['data'].toString();
-      });
-    });
-  }
+// 首页轮播组件
+class SwiperDiy extends StatelessWidget {
+  final List swiperDataList;
+  SwiperDiy({this.swiperDataList});
+  @override
+  Widget build(BuildContext context) {
 
-// 请求方法
-  Future getHttp() async {
-    try {
-      Response response;
-      Dio dio = new Dio();
-      dio.options.headers = httpHeaders;
-      response = await dio.get('https://time.geekbang.org/serv/v2/explore/all');
-      print(response);
-      return response;
-    } catch (e) {
-      return e;
-    }
+    // 适配组件 ScreenUtil
+    ScreenUtil.init(context,
+        designSize: Size(750, 1334), allowFontScaling: false);
+
+    return Container(
+        height: ScreenUtil().setHeight(170),
+        width: ScreenUtil().setWidth(750),
+        child: Swiper(
+          itemBuilder: (BuildContext context, int index) {
+            return Image.network("${swiperDataList[index]}");
+          },
+          itemCount: swiperDataList.length,
+          pagination: SwiperPagination(),
+          autoplay: true,
+        ),
+        color: Colors.orangeAccent);
   }
 }
+
+// class HomePage extends StatefulWidget {
+//   @override
+//   _HomePageState createState() => _HomePageState();
+// }
+
+// class _HomePageState extends State<HomePage> {
+//   String showText = '还没有请求数据';
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Container(
+//       child: Scaffold(
+//           appBar: AppBar(title: Text('请求远程数据')),
+//           // SingleChildScrollView 解决键盘或者其他滚动会有bug
+//           body: SingleChildScrollView(
+//             child: Column(
+//               children: <Widget>[
+//                 RaisedButton(
+//                   onPressed: _jike,
+//                   child: Text('请求数据'),
+//                 ),
+//                 Text(showText),
+//               ],
+//             ),
+//           )),
+//     );
+//   }
+
+//   void _jike() {
+//     print('开始向极客时间请求数据');
+//     getHttp().then((value) {
+//       setState(() {
+//         print(value);
+//         showText = value['data'].toString();
+//       });
+//     });
+//   }
+
+// // 请求方法
+//   Future getHttp() async {
+//     try {
+//       Response response;
+//       Dio dio = new Dio();
+//       dio.options.headers = httpHeaders;
+//       response = await dio.get('https://time.geekbang.org/serv/v2/explore/all');
+//       print(response);
+//       return response;
+//     } catch (e) {
+//       return e;
+//     }
+//   }
+// }
 
 // class HomePage extends StatelessWidget {
 //   @override
