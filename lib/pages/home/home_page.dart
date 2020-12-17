5de037095944bb168d89e5a9ca9b90e04b0fdd10
丰台_吴彦祖 <371251480@qq.com>
2020-12-17 17:12:37 +0800

    加载商品和上啦加载

--- lib/pages/home/home_page.dart
+++ lib/pages/home/home_page.dart
@@ -4,6 +4,8 @@ import '../../service/service_method.dart';
 import 'package:flutter_swiper/flutter_swiper.dart';
 import 'dart:convert';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:flutter_easyrefresh/easy_refresh.dart';
+import 'package:flutter_easyrefresh/bezier_bounce_footer.dart';
 
 class HomePage extends StatefulWidget {
   @override
@@ -17,13 +19,20 @@ class HomePage extends StatefulWidget {
 */
 class _HomePageState extends State<HomePage>
     with AutomaticKeepAliveClientMixin {
+  int page = 1;
+  List hotGoodsList = [];
+
   @override
   bool get wantKeepAlive => true;
 
   @override
   void initState() {
     super.initState();
+    // _getHotGoods();
   }
+
+  // GlobalKey<EasyRefreshState> _easyRefreshKey =new GlobalKey<EasyRefreshState>();
+  // GlobalKey<RefreshFooterState> _footerKey = new GlobalKey<RefreshFooterState>();
 
   String homePageContent = '正在获取数据';
 
@@ -44,11 +53,33 @@ class _HomePageState extends State<HomePage>
             String newsLeftBanner = data['data']['newsBanner'][0]['image'];
             String newsRightBanner = data['data']['newsBanner'][1]['image'];
             List recommendList = data['data']['recommend']; // 商品推荐
+            String floor1Title =
+                data['data']['floor1Pic']['PICTURE_ADDRESS']; //楼层1的标题图片
 
+            String floor2Title =
+                data['data']['floor2Pic']['PICTURE_ADDRESS']; //楼层1的标题图片
+            List floor2 = data['data']['floor2'];
             // List<Map> swiperDataList =
             //     (data['data']['slides'] as RList).cast(); // 顶部轮播组件数
-            return SingleChildScrollView(
-              child: Column(
+            return EasyRefresh(
+              // onRefresh: ClassicsFooter(
+              //     // key: _footerKey,
+              //     bgColor: Colors.white,
+              //     textColor: Colors.pink,
+              //     moreInfoColor: Colors.pink,
+              //     showMore: true,
+              //     noMoreText: '',
+              //     moreInfo: '加载中',
+              //     loadReadyText: '上拉加载....'
+              // ),
+              footer: ClassicalFooter(
+                bgColor: Colors.white,
+                textColor: Colors.redAccent,
+                noMoreText: '',
+                loadFailedText: '上拉加载....',
+              ),
+
+              child: ListView(
                 children: <Widget>[
                   SwiperDiy(swiperDataList: swiperDataList),
                   TopNavigator(navigatorList: navgatorList),
@@ -58,8 +89,25 @@ class _HomePageState extends State<HomePage>
                     newsLeftBanner: newsLeftBanner,
                     newsRightBanner: newsRightBanner,
                   ),
+                  FloorTitle(picture_address: floor1Title),
+                  FloorTitle(picture_address: floor2Title),
+                  FloorContent(floorGoodsList: floor2),
+                  _hotGoods(),
                 ],
               ),
+              onLoad: () async {
+                print('开始加载更多');
+                var formPage = {'page': page};
+                await request('homePageBelowConten', formData: formPage)
+                    .then((val) {
+                  var data = val;
+                  List newGoodsList = data['data'];
+                  setState(() {
+                    hotGoodsList.addAll(newGoodsList);
+                    page++;
+                  });
+                });
+              },
             );
             // return Text('qsdasdsa');
           } else {
@@ -70,6 +118,93 @@ class _HomePageState extends State<HomePage>
         },
       ),
     );
+  }
+
+  //火爆商品接口
+  void _getHotGoods() {
+    var formPage = {'page': page};
+    request('homePageBelowConten', formData: formPage).then((val) {
+      var data = val;
+      List newGoodsList = data['data'];
+      setState(() {
+        hotGoodsList.addAll(newGoodsList);
+        page++;
+      });
+    });
+  }
+
+  //火爆专区标题
+  Widget hotTitle = Container(
+    margin: EdgeInsets.only(top: 10.0),
+    padding: EdgeInsets.all(5.0),
+    alignment: Alignment.center,
+    decoration: BoxDecoration(
+        color: Colors.white,
+        border: Border(bottom: BorderSide(width: 0.5, color: Colors.black12))),
+    child: Text(
+      '猜你喜欢',
+    ),
+  );
+
+//火爆专区子项
+  Widget _wrapList() {
+    if (hotGoodsList.length != 0) {
+      List<Widget> listWidget = hotGoodsList.map((val) {
+        return InkWell(
+            onTap: () {
+              // Application.router.navigateTo(context,"/detail?id=${val['goodsId']}");
+            },
+            child: Container(
+              width: ScreenUtil().setWidth(372),
+              color: Colors.white,
+              padding: EdgeInsets.all(5.0),
+              margin: EdgeInsets.only(bottom: 3.0),
+              child: Column(
+                children: <Widget>[
+                  Image.network(
+                    val['image'],
+                    width: ScreenUtil().setWidth(375),
+                  ),
+                  Text(
+                    val['name'],
+                    maxLines: 1,
+                    overflow: TextOverflow.ellipsis,
+                    style: TextStyle(
+                        color: Colors.pink, fontSize: ScreenUtil().setSp(26)),
+                  ),
+                  Row(
+                    children: <Widget>[
+                      Text('￥${val['mallPrice']}'),
+                      Text(
+                        '￥${val['price']}',
+                        style: TextStyle(
+                            color: Colors.black26,
+                            decoration: TextDecoration.lineThrough),
+                      )
+                    ],
+                  )
+                ],
+              ),
+            ));
+      }).toList();
+      return Wrap(
+        spacing: 2,
+        children: listWidget,
+      );
+    } else {
+      return Text(' ');
+    }
+  }
+
+  //火爆专区组合
+  Widget _hotGoods() {
+    return Container(
+        child: Column(
+      children: <Widget>[
+        hotTitle,
+        _wrapList(),
+      ],
+    ));
   }
 }
 
@@ -261,4 +396,77 @@ class AdBanner extends StatelessWidget {
   }
 }
 
-// https://time.geekbang.org/serv/v2/explore/all
+class FloorTitle extends StatelessWidget {
+  final String picture_address; // 图片地址
+  FloorTitle({this.picture_address});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.all(8.0),
+      child: Image.network(this.picture_address),
+    );
+  }
+}
+
+// 楼层商品
+class FloorContent extends StatelessWidget {
+  final List floorGoodsList;
+
+  FloorContent({this.floorGoodsList});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: ScreenUtil().setHeight(360),
+      margin: EdgeInsets.only(left: 10, right: 10),
+      child: GridView.count(
+        physics: NeverScrollableScrollPhysics(),
+        crossAxisCount: 4,
+        // padding: EdgeInsets.all(5),
+        children: floorGoodsList.map((item) {
+          return _goodsItem(item);
+        }).toList(),
+      ),
+    );
+  }
+
+  Widget _goodsItem(item) {
+    return Container(
+      padding: EdgeInsets.only(top: 5),
+      decoration: BoxDecoration(
+        color: Colors.white,
+      ),
+      width: ScreenUtil().setWidth(375),
+      child: InkWell(
+        onTap: () {
+          // Application.router.navigateTo(context,)
+        },
+        child: Column(
+          children: <Widget>[
+            Container(
+              child: Text(
+                item['name'],
+                style: TextStyle(
+                  fontSize: 16,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+            Container(
+              child: Text(
+                item['descrition'],
+                style: TextStyle(fontSize: 13, color: Colors.grey),
+              ),
+            ),
+            Container(
+                child: Image.network(
+              item['image'],
+              width: ScreenUtil().setWidth(90),
+            )),
+          ],
+        ),
+      ),
+    );
+  }
+}
